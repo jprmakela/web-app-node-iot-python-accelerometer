6e31768d55a772e7e51d3b0177eb3df4f0fe9709
pc-adm <pc-adm>
2017-06-05 16:08:59 +0300

    fab lab

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'Fab Lab Temperature & Humidity Real-time Data',
       fontSize: 36
     },
     scales: {
