a733e301a1811bfa73051066ca4a22c078ec0d25
pc-adm <pc-adm>
2017-07-31 12:35:45 +0300

    Acc Mar and Rot data

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,115 +1,231 @@
 $(document).ready(function () {
-  var timeData = [],
-    xAxisData = [],
-    yAxisData = [],
-    zAxisData = [];
-  var data = {
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'X-Axis',
-        yAxisID: 'yAxis',
-        borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
-        backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: xAxisData
-      },
+    var timeData = [],
+        xAxisAccData = [],
+        yAxisAccData = [],
+        zAxisAccData = [],
+        xAxisMagData = [],
+        yAxisMagData = [],
+        zAxisMagData = [],
+        xAxisRotData = [],
+        yAxisRotData = [],
+        zAxisRotData = [];
+
+    var accData = {
+        labels: timeData,
+        datasets: [
             {
-        fill: false,
-        label: 'Y-Axis',
-        yAxisID: 'yAxis',
-        borderColor: "rgba(219, 50, 54, 1)",
-        pointBoarderColor: "rgba(219, 50, 54, 1)",
-        backgroundColor: "rgba(219, 50, 54, 0.4)",
-        pointHoverBackgroundColor: "rgba(219, 50, 54, 1)",
-        pointHoverBorderColor: "rgba(219, 50, 54, 1)",
-        data: yAxisData
-      },
-      {
-        fill: false,
-        label: 'Z-Axis',
-        yAxisID: 'yAxis',
-        borderColor: "rgba(24, 120, 240, 1)",
-        pointBoarderColor: "rgba(24, 120, 240, 1)",
-        backgroundColor: "rgba(24, 120, 240, 0.4)",
-        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
-        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: zAxisData
-      }
-    ]
-  }
-
-  var basicOption = {
-    title: {
-      display: true,
-      text: 'Fab Lab Accelerometer',
-      fontSize: 36
-    },
-    scales: {
-      yAxes: [{
-        id: 'yAxis',
-        type: 'linear',
-        scaleLabel: {
-          labelString: 'g',
-          display: true
-        },
-        position: 'right',
-      }]
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: xAxisAccData
+            },
+            {
+                fill: false,
+                label: 'Y-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(219, 50, 54, 1)",
+                pointBoarderColor: "rgba(219, 50, 54, 1)",
+                backgroundColor: "rgba(219, 50, 54, 0.4)",
+                pointHoverBackgroundColor: "rgba(219, 50, 54, 1)",
+                pointHoverBorderColor: "rgba(219, 50, 54, 1)",
+                data: yAxisAccData
+            },
+            {
+                fill: false,
+                label: 'Z-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(24, 120, 240, 1)",
+                pointBoarderColor: "rgba(24, 120, 240, 1)",
+                backgroundColor: "rgba(24, 120, 240, 0.4)",
+                pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+                pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+                data: zAxisAccData
+            }
+        ]
     }
-  }
 
-  //Get the context of the canvas element we want to select
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var optionsNoAnimation = { animation: false }
-  var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: basicOption
-  });
-
-  var ws = new WebSocket('wss://' + location.host);
-  ws.onopen = function () {
-    console.log('Successfully connect WebSocket');
-  }
-  ws.onmessage = function (message) {
-    console.log('receive message' + message.data);
-    try {
-      var obj = JSON.parse(message.data);
-      if (!obj.time || !obj.xAxis) {
-        return;
-      }
-      // only keep no more than 50 points in the line chart
-      const maxLen = 50;
-
-      timeData.push(obj.time);
-
-      xAxisData.push(obj.xAxis);
-      var len = timeData.length;
-      if (len > maxLen) {
-        timeData.shift();
-        xAxisData.shift();
-      }
-
-      if (obj.yAxis) {
-        yAxisData.push(obj.yAxis);
-      }
-      if (yAxisData.length > maxLen) {
-        yAxisData.shift();
-      }
-
-      if (obj.zAxis) {
-        zAxisData.push(obj.zAxis);
-      }
-      if (zAxisData.length > maxLen) {
-        zAxisData.shift();
-      }
-
-      myLineChart.update();
-    } catch (err) {
-      console.error(err);
+    var magData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: xAxisMagData
+            },
+            {
+                fill: false,
+                label: 'Y-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(219, 50, 54, 1)",
+                pointBoarderColor: "rgba(219, 50, 54, 1)",
+                backgroundColor: "rgba(219, 50, 54, 0.4)",
+                pointHoverBackgroundColor: "rgba(219, 50, 54, 1)",
+                pointHoverBorderColor: "rgba(219, 50, 54, 1)",
+                data: yAxisMagData
+            },
+            {
+                fill: false,
+                label: 'Z-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(24, 120, 240, 1)",
+                pointBoarderColor: "rgba(24, 120, 240, 1)",
+                backgroundColor: "rgba(24, 120, 240, 0.4)",
+                pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+                pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+                data: zAxisMagData
+            }
+        ]
     }
-  }
-});
+
+    var rotData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: xAxisRotData
+            },
+            {
+                fill: false,
+                label: 'Y-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(219, 50, 54, 1)",
+                pointBoarderColor: "rgba(219, 50, 54, 1)",
+                backgroundColor: "rgba(219, 50, 54, 0.4)",
+                pointHoverBackgroundColor: "rgba(219, 50, 54, 1)",
+                pointHoverBorderColor: "rgba(219, 50, 54, 1)",
+                data: yAxisRotData
+            },
+            {
+                fill: false,
+                label: 'Z-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(24, 120, 240, 1)",
+                pointBoarderColor: "rgba(24, 120, 240, 1)",
+                backgroundColor: "rgba(24, 120, 240, 0.4)",
+                pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+                pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+                data: zAxisRotData
+            }
+        ]
+    }
+
+    var basicOption = {
+        responsive: true,
+        scales: {
+            yAxes: [{
+                id: 'yAxis',
+                type: 'linear',
+                scaleLabel: {
+                    labelString: 'g',
+                    display: true
+                },
+                position: 'right',
+            }]
+        }
+    }
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("accChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var accLineChart = new Chart(ctx, {
+        type: 'line',
+        data: accData,
+        options: basicOption
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("magChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var magLineChart = new Chart(ctx, {
+        type: 'line',
+        data: accData,
+        options: basicOption
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("rotChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var rotLineChart = new Chart(ctx, {
+        type: 'line',
+        data: accData,
+        options: basicOption
+    });
+
+    var ws = new WebSocket('wss://' + location.host);
+    ws.onopen = function () {
+        console.log('Successfully connect WebSocket');
+    }
+
+    ws.onmessage = function (message) {
+        console.log('receive message' + message.data);
+        try {
+            var obj = JSON.parse(message.data);
+            if (!obj.time || !obj.xAxisAcc) {
+                return;
+            }
+            // only keep no more than 50 points in the line chart
+            const maxLen = 50;
+
+            timeData.push(obj.time);
+
+            xAxisAccData.push(obj.xAxisAcc);
+            xAxisMagData.push(obj.yAxisMag);
+            xAxisRotData.push(obj.yAxisRot);
+            
+            var len = timeData.length;
+            if (len > maxLen) {
+                timeData.shift();
+                xAxisAccData.shift();
+                xAxisMagData.shift();
+                yAxisRotData.shift();
+            }
+
+            if (obj.yAxisAcc) {
+                yAxisAccData.push(obj.yAxisAcc);
+                yAxisMagData.push(obj.yAxisMag);
+                yAxisRotData.push(obj.yAxisRot);
+            }
+            if (yAxisAccData.length > maxLen) {
+                yAxisAccData.shift();
+                yAxisMagData.shift();
+                yAxisRotData.shift();
+            }
+
+            if (obj.zAxisAcc) {
+                zAxisAccData.push(obj.zAxisAcc);
+                zAxisMagData.push(obj.zAxisMag);
+                zAxisRotData.push(obj.zAxisRot);
+            }
+            if (zAxisAccData.length > maxLen) {
+                zAxisAccData.shift();
+                zAxisMagData.shift();
+                zAxisRotData.shift();
+            }
+
+            accLineChart.update();
+            magLineChart.update();
+            rotLineChart.update();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+})
