9cffead6741291880c1aa8aaab5121a6c7a2b4d1
pc-adm <pc-adm>
2017-08-11 11:48:18 +0300

    Added all sensors

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,17 @@ $(document).ready(function () {
         zAxisMagData = [],
         xAxisRotData = [],
         yAxisRotData = [],
-        zAxisRotData = [];
+        zAxisRotData = [],
+
+        objTemperatureData = [],
+        ambTemperatureData = [],
+
+        lightmeterData = [],
+
+        barometerData = [],
+
+        humidityData = [];
+
 
     var accData = {
         labels: timeData,
@@ -127,8 +137,108 @@ $(document).ready(function () {
         ]
     }
 
-    var basicOption = {
+    var temData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'Ambience',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: objTemperatureData
+            },
+            {
+                fill: false,
+                label: 'Object',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(219, 50, 54, 1)",
+                pointBoarderColor: "rgba(219, 50, 54, 1)",
+                backgroundColor: "rgba(219, 50, 54, 0.4)",
+                pointHoverBackgroundColor: "rgba(219, 50, 54, 1)",
+                pointHoverBorderColor: "rgba(219, 50, 54, 1)",
+                data: ambTemperatureData
+            },
+        ]
+    }
+
+    var preData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: barometerData
+            },
+        ]
+    }
+
+    var humData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: humidityData
+            },
+        ]
+    }
+
+    var lightData = {
+        labels: timeData,
+        datasets: [
+            {
+                fill: false,
+                label: 'X-Axis',
+                yAxisID: 'yAxis',
+                borderColor: "rgba(255, 204, 0, 1)",
+                pointBoarderColor: "rgba(255, 204, 0, 1)",
+                backgroundColor: "rgba(255, 204, 0, 0.4)",
+                pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+                pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+                data: lightmeterData
+            },
+        ]
+    }
+
+    var OptionWithLabels = {
         responsive: true,
+        legend: {
+            display: true
+        },
+        scales: {
+            yAxes: [{
+                id: 'yAxis',
+                type: 'linear',
+                scaleLabel: {
+                    labelString: 'g',
+                    display: true
+                },
+                position: 'right',
+            }]
+        }
+    }
+
+    var OptionWithoutLabels = {
+        responsive: true,
+        legend: {
+            display: false
+        },
         scales: {
             yAxes: [{
                 id: 'yAxis',
@@ -148,7 +258,7 @@ $(document).ready(function () {
     var accLineChart = new Chart(ctx, {
         type: 'line',
         data: accData,
-        options: basicOption
+        options: OptionWithLabels
     });
 
     //Get the context of the canvas element we want to select
@@ -157,7 +267,7 @@ $(document).ready(function () {
     var magLineChart = new Chart(ctx, {
         type: 'line',
         data: magData,
-        options: basicOption
+        options: OptionWithLabels
     });
 
     //Get the context of the canvas element we want to select
@@ -166,7 +276,43 @@ $(document).ready(function () {
     var rotLineChart = new Chart(ctx, {
         type: 'line',
         data: rotData,
-        options: basicOption
+        options: OptionWithLabels
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("temChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var temLineChart = new Chart(ctx, {
+        type: 'line',
+        data: temData,
+        options: OptionWithLabels
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("preChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var preLineChart = new Chart(ctx, {
+        type: 'line',
+        data: preData,
+        options: OptionWithoutLabels
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("humChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var humLineChart = new Chart(ctx, {
+        type: 'line',
+        data: humData,
+        options: OptionWithoutLabels
+    });
+
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("lightChart").getContext("2d");
+    var optionsNoAnimation = { animation: false }
+    var lightLineChart = new Chart(ctx, {
+        type: 'line',
+        data: lightData,
+        options: OptionWithoutLabels
     });
 
     var ws = new WebSocket('wss://' + location.host);
@@ -181,7 +327,7 @@ $(document).ready(function () {
             if (!obj.time || !obj.accelerometer[0]) {
                 return;
             }
-            // only keep no more than 50 points in the line chart
+            // only keep no more than 15 points in the line chart
             const maxLen = 15;
 
             timeData.push(obj.time);
@@ -189,27 +335,42 @@ $(document).ready(function () {
             xAxisAccData.push(obj.accelerometer[0]);
             xAxisMagData.push(obj.magnetometer[0]);
             xAxisRotData.push(obj.gyroscope[0]);
+            objTemperatureData.push(obj.IRtemperature[0]);
+            lightmeterData.push(obj.lightmeter);
             
+            
+
+
             var len = timeData.length;
             if (len > maxLen) {
                 timeData.shift();
                 xAxisAccData.shift();
                 xAxisMagData.shift();
                 xAxisRotData.shift();
+                objTemperatureData.shift();
+                lightmeterData.shift();
+
             }
 
-            if (obj.accelerometer[1]) {
+            if (obj.yAxisAcc) {
                 yAxisAccData.push(obj.accelerometer[1]);
                 yAxisMagData.push(obj.magnetometer[1]);
                 yAxisRotData.push(obj.gyroscope[1]);
+                ambTemperatureData.push(obj.IRtemperature[1]);
+                barometerData.push(obj.barometer[1]);
+                humidityData.push(obj.humidity[1]);
+
             }
             if (yAxisAccData.length > maxLen) {
                 yAxisAccData.shift();
                 yAxisMagData.shift();
                 yAxisRotData.shift();
+                ambTemperatureData.shift();
+                barometerData.shift();
+                humidityData.shift();
             }
 
-            if (obj.accelerometer[2]) {
+            if (obj.zAxisAcc) {
                 zAxisAccData.push(obj.accelerometer[2]);
                 zAxisMagData.push(obj.magnetometer[2]);
                 zAxisRotData.push(obj.gyroscope[2]);
@@ -223,6 +384,11 @@ $(document).ready(function () {
             accLineChart.update();
             magLineChart.update();
             rotLineChart.update();
+            temLineChart.update();
+            preLineChart.update();
+            humLineChart.update();
+            lightLineChart.update();
+
         } catch (err) {
             console.error(err);
         }
